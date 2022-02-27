3b096b59af94bbc74ebf28e6100ed5da3b3b8e27
John Hörnvall <trolledwoods@gmail.com>
2022-02-27 15:19:33 +0100

    You can make keys not disappear after you unlock a door, and a prompt when you pick up an item

--- Assets/Scripts/Items/Key.cs
+++ Assets/Scripts/Items/Key.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : Item {
-    public override string name { get => this.color == Color.Red ? "Red key" : "Blue key"; }
+    public override string name { get => (this.color == Color.Red ? "Red key" : "Blue key") + (singleUse ? " (once)" : ""); }
+    public bool singleUse = true;
 
     public enum Color { Red, Blue };
 
